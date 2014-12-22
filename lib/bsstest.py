3c19d038213b01a0990a027ae5b34c33b0557553
Matthew Glazar <strager@fb.com>
2014-12-22 00:01:53 -0800

    Put the multiplot's legend on the bottom

--- lib/bsstest.py
+++ lib/bsstest.py
@@ -65,30 +65,53 @@ class PlotFile(object):
         self.__file = plot_file
 
     @staticmethod
-    def sanitize(string):
-        return string  # TODO(strager)
+    def __sanitize_string(value):
+        return str(value)  # TODO(strager)
+
+    @staticmethod
+    def sanitize(value):
+        if isinstance(value, basestring):
+            return '"{}"'.format(
+                PlotFile.__sanitize_string(value),
+            )
+        elif isinstance(value, tuple):
+            return ', '.join(map(PlotFile.sanitize, value))
+        elif value == float(value) \
+                and not isinstance(value, bool):
+            return str(float(value))
+        else:
+            return 'on' if value else 'off'
 
     @staticmethod
     def gnuplot(plot_file_name):
         subprocess.check_call(['gnuplot', plot_file_name])
 
-    def begin_multiplot(self, layout=None):
-        self.__file.write('set multiplot')
-        if layout is not None:
-            self.__file.write(' layout {},{}'.format(
-                PlotFile.sanitize(int(layout[0])),
-                PlotFile.sanitize(int(layout[1])),
-            ))
-        self.__file.write('\n')
+    def write_raw(self, string):
+        self.__file.write(string)
 
-    def end_multiplot(self):
-        self.__file.write('unset multiplot\n')
+    def write_set(self, name, value=None, kwoptions=[]):
+        self.write_raw('set {}'.format(
+            PlotFile.__sanitize_string(name),
+        ))
+        if value is not None:
+            self.write_raw(' {}'.format(
+                PlotFile.sanitize(value),
+            ))
+        for (name, value) in kwoptions:
+            self.write_raw(' {} {}'.format(
+                PlotFile.__sanitize_string(name),
+                PlotFile.sanitize(value),
+            ))
+        self.write_raw('\n')
+
+    def write_unset(self, name):
+        self.write_raw('unset {}'.format(
+            PlotFile.__sanitize_string(name),
+        ))
 
     def write_gif_header(self, output_file_name):
-        self.__file.write('set terminal gif\n')
-        self.__file.write('set output "{}"\n'.format(
-            PlotFile.sanitize(output_file_name),
-        ))
+        self.write_set('terminal', 'gif')
+        self.write_set('output', output_file_name)
 
     def write_plot(
         self,
@@ -104,24 +127,22 @@ class PlotFile(object):
         }
         for (name, value) in options.iteritems():
             if value is not None:
-                self.__file.write('set {} "{}"\n'.format(
-                    PlotFile.sanitize(name),
-                    PlotFile.sanitize(value),
-                ))
-        self.__file.write('plot {}\n'.format(
+                self.write_set(name, str(value))
+        self.write_raw('plot {}\n'.format(
             ', '.join(
-                '"-" title "{}" with linespoints'
-                    .format(PlotFile.sanitize(series_name))
+                '"-" title {} with linespoints'.format(
+                    PlotFile.sanitize(str(series_name)),
+                )
                 for series_name in series_points.iterkeys()
             )
         ))
         for points in series_points.itervalues():
             for (x, y) in points:
-                self.__file.write(' '.join([
-                    PlotFile.sanitize(str(x)),
-                    PlotFile.sanitize(str(y)),
+                self.write_raw(' '.join([
+                    PlotFile.sanitize(x),
+                    PlotFile.sanitize(y),
                 ]) + '\n')
-            self.__file.write('e\n')
+            self.write_raw('e\n')
 
     def flush(self):
         self.__file.flush()
@@ -206,12 +227,37 @@ def main(class_groups):
         for cls in classes:
             cls._augment_arg_parser(parser)
     args = parser.parse_args()
+
     with temp_plot_file() as plot_file:
         plot_file.write_gif_header('plot.gif')
-        plot_file.begin_multiplot(
-            layout=layout_for_plot_count(len(class_groups)),
+
+        # Shift all the plots up to make room for the
+        # legend.
+        bottom_offset = 0.030
+        scale_factor = 1 - bottom_offset / 2
+        plot_file.write_set(
+            'multiplot',
+            kwoptions=[
+                ('layout', layout_for_plot_count(
+                    len(class_groups),
+                )),
+                ('scale', (scale_factor, scale_factor)),
+                ('offset', (0, bottom_offset)),
+            ]
         )
+
+        first = True
         for classes in class_groups:
+            if first:
+                # Show the first plot's legend at the
+                # bottom.
+                plot_file.write_raw(
+                    'set key at screen 0.5, screen 0.0 '
+                    'center bottom maxrows 1\n'
+                )
+                first = False
+
             test_data = list(yield_test_data(classes, args))
             plot_test_data(plot_file, test_data)
-        plot_file.end_multiplot()
+            plot_file.write_set('key', False)
+        plot_file.write_unset('multiplot')
